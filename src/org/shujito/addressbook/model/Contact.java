0597fbbe077a644f071d2028a0ca3450ddbb2e1d
Alberto Ramos <shujito.desu@gmail.com>
2014-09-10 00:39:25 -0600

    - putting some validation on the model - made delete button one level deeper (one more press)

--- src/org/shujito/addressbook/model/Contact.java
+++ src/org/shujito/addressbook/model/Contact.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Column.ConflictAction;
 import com.activeandroid.annotation.Table;
 
 /**
@@ -17,19 +18,22 @@ public class Contact extends Model implements Serializable
 	/* statics */
 	private static final long serialVersionUID = 1L;
 	public static final String TABLE_CONTACT = "contact";
+	public static final String CONTACT_ID = "_id";
 	public static final String CONTACT_NAME = "name";
 	public static final String CONTACT_LASTNAME = "lastname";
 	public static final String CONTACT_ADDRESS = "address";
 	public static final String CONTACT_PHONE = "phone";
 	public static final String CONTACT_NOTES = "notes";
 	/* fields */
-	@Column(name = CONTACT_NAME)
+	@Column(name = CONTACT_ID, unique = true, onUniqueConflict = ConflictAction.REPLACE)
+	public String id = null;
+	@Column(name = CONTACT_NAME, notNull = true)
 	public String name = null;
 	@Column(name = CONTACT_LASTNAME)
 	public String lastname = null;
 	@Column(name = CONTACT_ADDRESS)
 	public String address = null;
-	@Column(name = CONTACT_PHONE)
+	@Column(name = CONTACT_PHONE, notNull = true)
 	public String phone = null;
 	@Column(name = CONTACT_NOTES)
 	public String notes = null;
